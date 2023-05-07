03e00cc45092e176680a27de39872df870c7f206
Abhinav Parbhakar <abhinav.parbhakar302@gmail.com>
2023-05-06 20:12:44 -0600

    Added funcitonality to get the path of the download and checking if Java is downloaded

--- script.sh
+++ script.sh
@@ -1,2 +1,12 @@
-#! /usr/bin/bash
-echo "Hello World"
+#! /bin/bash
+sudo apt-get install mlocate
+sudo apt autoremove
+ZIPLOCATION=$(locate "The Tokarsky-Marinov Finder, Tester and Prover.zip" |grep '/Downloads/')
+java
+if [ $? -ne 0 ]; then
+    echo "Java not installed on this machine. Installing Java..."
+    
+fi
+
+
+
