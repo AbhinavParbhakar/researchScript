5b1f8b5328e05dd014feb691a180f2f76550a8d8
Abhinav Parbhakar <abhinav.parbhakar302@gmail.com>
2023-05-07 14:03:01 -0600

    added java testing

--- script.sh
+++ script.sh
@@ -4,9 +4,22 @@ sudo apt autoremove
 ZIPLOCATION=$(locate "The Tokarsky-Marinov Finder, Tester and Prover.zip" |grep '/Downloads/')
 java
 if [ $? -ne 0 ]; then
-    echo "Java not installed on this machine. Installing Java..."
+    echo "Java not installed on this machine. Please Install Java:"
+    echo "Use the following link:"
+    echo "https://www.oracle.com/ca-en/java/technologies/javase/javase8-archive-downloads.html"
+    echo -e "\nDownload the 8u66 Development kit"
+    echo "Download the file that ends in 'linux-x64.tar.gz'"
     
+    JAVALOCATION=$(locate "jdk-8u66-linux-x64.tar.gz" | grep "/Downloads/")
+    
+    sudo mkdir -p /opt/jdk
+    sudo cp -rf $JAVALOCATION /opt/jdk/
+    cd /opt/jdk/
+    sudo tar -zxf "jdk-8u66-linux-x64.tar.gz"
 fi
+
+JAVAVERSION=$(java -version 2>&1)
+echo "$JAVAVERSION"
 
 
 
